c73e15989153cb7cc9c45eadfd98c1bdfa213056
terycake <kfjdds222000999@gmail.com>
2023-12-06 00:39:43 +0300

    Revert "parser for .conf file"
    This reverts commit 83d307c2adb04935876f075a910ce6daf166b7e3.

---
+++ src/config/interpreter.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace config {
+    void read_config();
+}
---
+++ src/config/options.h
@@ -0,0 +1,25 @@
+#pragma once 
+
+#include <string>
+#include <map>
+#include <variant>
+#include <vector>
+#include "../tui/tui.h"
+
+namespace config {
+    enum class OPTIONS_TYPE{
+        INT,
+        BOOL,
+        COLOR,
+        CUSTOM
+    };
+
+    using config_t = std::variant<int, std::string, tui::Color, bool>;
+
+    extern std::map<std::string, std::vector<std::string>> custom_options;
+    extern std::map<std::string, OPTIONS_TYPE> options;
+    extern std::map<std::string, config_t> config;
+
+    void setup_options();
+
+}
